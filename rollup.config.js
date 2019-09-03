e17e212e46a0bdb9fcbcaeac71bb9b34b7603336
Sergio de la Garza <sergio@delagarza.io>
2019-09-02 19:15:08 -0500

    Remove compiled test files and rollup config for compiling tests

--- rollup.config.js
+++ rollup.config.js
@@ -49,29 +49,5 @@ export default [
         exclude: "node_modules/**"
       })
     ]
-  },
-  {
-    input: "tests/**/*.test.js",
-    output: {
-      file: "dist/tests.bundle.js",
-      name: "lib",
-      sourcemap: true,
-      format: "iife",
-      banner,
-      globals: {
-        chai: "chai",
-        it: "it",
-        describe: "describe"
-      }
-    },
-    external: ["chai", "it", "describe"],
-    plugins: [
-      resolve(),
-      commonjs(),
-      babel({
-        exclude: "node_modules/**"
-      }),
-      multiEntry()
-    ]
   }
 ];
